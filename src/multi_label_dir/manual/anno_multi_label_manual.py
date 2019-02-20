e6accee20c680788e2d801c5a32146f243fe6cb3
shuu-tatsu <helloshuushuu@gmail.com>
2019-02-20 18:02:27 +0900

    init

---
+++ src/multi_label_dir/manual/anno_multi_label_manual.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tqdm import tqdm
+from flashtext import KeywordProcessor
+import random
+
+
+class Annotation():
+
+    #def __init__(self, TRAIN_FILE, DEV_FILE, EVAL_FILE, dict_list):
+    def __init__(self, TRAIN_FILE, DEV_FILE, dict_list):
+        self.train_file = TRAIN_FILE
+        self.dev_file = DEV_FILE
+        #self.eval_file = EVAL_FILE
+        self.gene_list = self.load(dict_list[0])
+        self.protein_list = self.load(dict_list[1])
+
+    def load(self, dict_file):
+        with open(dict_file, 'r') as r:
+            vocab_list = [word.strip('\n') for word in r]
+        return vocab_list
+
+    def make_annotation(self):
+        make_anno_corpus(self.gene_list, self.protein_list, self.train_file)
+        make_anno_corpus(self.gene_list, self.protein_list, self.dev_file)
+        #make_anno_corpus(self.gene_list, self.protein_list, self.eval_file)
+
+
+def make_anno_corpus(gene_list, protein_list, target_corpus):
+    #NE抽出器生成
+    #遺伝子は大文字小文字の区別あり、タンパク質は区別なし
+    gene_processor = make_ne_founder(gene_list, case=True)
+    protein_processor = make_ne_founder(protein_list, case=False)
+    #データからNEを抽出
+    extracted_words_list = extract_keywords(target_corpus, gene_processor, protein_processor) 
+    #Write annotation file
+    write_file = target_corpus[:56] + 'anno_data/anno_' + target_corpus[67:]
+    write_annotation(extracted_words_list, write_file)
+
+
+def write_annotation(extracted_words_list, write_file):
+    with open(write_file, 'w') as w:
+
+        #all sentences
+        for keywords_found_list in extracted_words_list:
+
+            #one sentence
+            for sent_id, keyword_found, tag_type in keywords_found_list:
+                w.write(sent_id + '\t')
+                w.write(str(keyword_found[0]) + '\t')
+                w.write(str(keyword_found[1]) + '\t')
+                w.write(str(keyword_found[2]) + '\t')
+                w.write(tag_type)
+                w.write('\n')
+
+
+def make_ne_founder(vocab_list, case):
+    keyword_processor = KeywordProcessor(case_sensitive=case)
+    for vocab in tqdm(vocab_list):
+        keyword_processor.add_keyword(vocab)
+    return keyword_processor
+
+
+def extract_keywords(target_corpus, gene_processor, protein_processor):
+    with open(target_corpus, 'r') as r:
+        sentences = [sentence.split('\t') for sentence in r]
+    extracted_words_list = []
+    for sent_id, sentence in tqdm(sentences):
+        gene_found_list = gene_processor.extract_keywords(sentence, span_info=True)
+        protein_found_list = protein_processor.extract_keywords(sentence, span_info=True)
+
+        sorted_list = sort_list(sent_id, gene_found_list, protein_found_list)
+        extracted_words_list.append(sorted_list)
+    return extracted_words_list
+
+
+def sort_list(sent_id, gene_found_list, protein_found_list):
+    #同一センテンス内でのソート
+    temp_list = []
+    temp_list.extend([(sent_id, gene_found, 'gene') for gene_found in gene_found_list])
+    temp_list.extend([(sent_id, protein_found, 'protein') for protein_found in protein_found_list])
+    sorted_list = sorted(temp_list, key=lambda x: x[1][1])
+    return sorted_list
+
+
+#def labeling_id(target_file, train_file, dev_file, eval_file):
+def labeling_id(target_file, train_file, dev_file):
+    #file_list = [train_file, dev_file, eval_file]
+    file_list = [train_file, dev_file]
+    for data_file in file_list:
+        write_file = target_file + 'id_data/id_' + data_file[(len(target_file)+5):]
+        with open(data_file, 'r') as r, open(write_file, 'w') as w:
+            sentence_id = 1
+            for sentence in r:
+                line = str(sentence_id) + '\t' + sentence
+                w.write(line)
+                sentence_id += 1
+
+
+def main(TARGET_FILE, dict_list):
+    make_id_corpus = True
+    if make_id_corpus:
+        TRAIN_FILE = TARGET_FILE + 'orig/train.txt' 
+        DEV_FILE = TARGET_FILE + 'orig/valid.txt'
+        #EVAL_FILE = TARGET_FILE + 'orig/test.txt'
+        #labeling_id(TARGET_FILE, TRAIN_FILE, DEV_FILE, EVAL_FILE)
+        labeling_id(TARGET_FILE, TRAIN_FILE, DEV_FILE)
+
+    make_anno_corpus = True
+    if make_anno_corpus:
+        TRAIN_FILE = TARGET_FILE + 'id_data/id_train.txt' 
+        DEV_FILE = TARGET_FILE + 'id_data/id_valid.txt'
+        #EVAL_FILE = TARGET_FILE + 'id_data/id_test.txt'
+
+        #anno = Annotation(TRAIN_FILE, DEV_FILE, EVAL_FILE, dict_list)
+        anno = Annotation(TRAIN_FILE, DEV_FILE, dict_list)
+        anno.make_annotation()
+
+
+if __name__ == '__main__':
+    #TARGET_FILE = '/cl/work/shusuke-t/BioIE/data/multi_label_corpus/dsv/'
+    TARGET_FILE = '/cl/work/shusuke-t/BioIE/data/multi_label_corpus/manual/'
+    GENE = '/cl/work/shusuke-t/BioIE/2019_02_05_data_nomura/gene/ecocyc/gene_ecocyc.txt'
+    PROTEIN = '/cl/work/shusuke-t/BioIE/2019_02_12_parse_drugbank_ipynb/data/protein_drugbank.txt'
+    dict_list = [GENE, PROTEIN]
+    main(TARGET_FILE, dict_list)
---
+++ src/multi_label_dir/manual/convert_htmlanno.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import os
+import os.path
+import re
+
+
+class Converter():
+
+    def __init__(self, file_list):
+        self.read_dir = file_list[0]
+        self.id_data_dir = file_list[1]
+        self.anno_data_dir = file_list[2]
+
+    def convert(self):
+        #read
+        xhtml_list = self.read('.xhtml')
+        htmlanno_list = self.read('.htmlanno')
+
+        #write
+        self.write_id(xhtml_list)
+        self.write_anno(htmlanno_list)
+
+    def read(self, ext_type):
+        files = []
+        for f in os.listdir(self.read_dir):
+            if os.path.isfile(self.read_dir + f):
+                root, ext = os.path.splitext(self.read_dir + f)
+                if ext == ext_type:
+                    files.append(f)
+        return files
+
+    def write_id(self, abst_list):
+        '''
+        <h1>[改行][タイトル][改行]</h1>[改行]<p>[改行]本文[改行]</p>
+        '''
+        #ファイル読み込み
+        extracted_list = []
+        for abst_file in abst_list:
+            abst_id = abst_file[:abst_file.find('.')]
+
+            with open(self.read_dir + abst_file, 'r') as r:
+                xhtml_str = r.read()
+
+            #HTMLを解析(BeautifulSoup)
+            soup = BeautifulSoup(xhtml_str, 'html.parser')
+            title_str = soup.html.body.h1
+            abst_str = soup.html.body.p
+            #改行をスペースに変換
+            text_str = (title_str.string + abst_str.string).replace('\n','\s')
+            extracted_list.append((abst_id, text_str))
+
+        #ファイル書き込み
+        id_data_file = 'id_test.txt'
+        with open(self.id_data_dir + id_data_file, 'w') as w:
+            for abst_id, text_str in extracted_list:
+                written_str = abst_id + '\t' + text_str + '\n'
+                w.write(written_str)
+
+    def write_anno(self, anno_list):
+        '''
+        条件:
+        type = "span"
+        label = "gene" or "protein"
+        '''
+        extracted_anno_list = []
+        for anno_file in anno_list:
+            anno_id = anno_file[:anno_file.find('.')]
+            with open(self.read_dir + anno_file, 'r') as r:
+                anno_parser = AnnoParser(r)
+                #one abst（同一anno_id）における、アノテーション情報
+                anno_info_list = anno_parser.read()
+            extracted_anno_list.append((anno_id, anno_info_list))
+
+        #ファイル書き込み
+        anno_data_file = 'anno_test.txt'
+        with open(self.anno_data_dir + anno_data_file, 'w') as w:
+            for abst_id, anno_info_list in extracted_anno_list:
+                for anno_info in anno_info_list:
+                    anno_type = anno_info[0][0]
+                    position = anno_info[1][0]
+                    try:
+                        s_posi = position.strip('[], ').split(',')[0]
+                        e_posi = position.strip('[], ').split(',')[1]
+                    except IndexError:
+                        s_posi = -1
+                        e_posi = -1
+                    ne_name = anno_info[2][0]
+                    label = anno_info[3][0]
+                    #条件でフィルタリング
+                    if anno_type == 'span':
+                        if label == 'gene' or label == 'protein':
+                            written_str = abst_id + '\t' + ne_name + '\t' + \
+                                          s_posi + '\t' + e_posi + '\t' + label + '\n'
+                            w.write(written_str)
+
+
+'''
+AnnoParser
+解析対象：abst 1つ
+'''
+class AnnoParser():
+
+    def __init__(self, r):
+        self.r = r
+
+    def read(self):
+        lines = [line for line in self.r]
+        start_index_list = self.get_start_index(lines)
+        tuple_index_list = self.get_tuple_index(start_index_list)
+        extracted_info_list = self.parse(lines, tuple_index_list)
+        return extracted_info_list
+
+    def parse(self, lines, tuple_index_list):
+        extracted_info_list = []
+        for s_index, e_index in tuple_index_list:
+            entity = lines[s_index:e_index]
+            entity_info_list = []
+            for anno_info in entity:
+                info_str = re.sub('[\n\s"]', '', anno_info).split('=')[-1:]
+                entity_info_list.append(info_str)
+            extracted_info_list.append(entity_info_list)
+        return extracted_info_list
+
+    def get_tuple_index(self, start_index_list):
+        tuple_index_list = [(s, s + 4) for s in start_index_list]
+        return tuple_index_list
+
+    def get_start_index(self, lines):
+        start_index_list = []
+        for i, line in enumerate(lines):
+            if line.find('type') == 0:
+                start_index_list.append(i)
+        return start_index_list
+
+
+def main():
+    # data
+    READ_DIR = '/cl/work/shusuke-t/BioIE/2019_02_05_data_nomura/manual_anno0220/'
+    WRITE_DIR = '/cl/work/shusuke-t/BioIE/data/multi_label_corpus/manual/'
+    ANNO_DATA_DIR = WRITE_DIR + 'anno_data/'
+    ID_DATA_DIR = WRITE_DIR + 'id_data/'
+    file_list = [READ_DIR, ID_DATA_DIR, ANNO_DATA_DIR]
+
+    # convert
+    converter = Converter(file_list)
+    converter.convert()
+
+
+if __name__ == '__main__':
+    main()
